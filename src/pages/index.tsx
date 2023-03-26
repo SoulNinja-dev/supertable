b2bebea3e5df110b7dc3f65ed2432da188eab59a
milan090 <milanmuhammed1@gmail.com>
2023-03-26 12:19:52 +0530

    fix migrations and redirect on login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
-import { type NextPage } from "next";
+import {
+  GetServerSideProps,
+  InferGetServerSidePropsType,
+  type NextPage,
+} from "next";
 import Head from "next/head";
 
 import { motion } from "framer-motion";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "~/server/auth";
 
-const Home: NextPage = () => {
+const Home: NextPage<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> = ({ msg }) => {
+  const session = useSession();
+
   return (
     <>
       <Head>
@@ -19,21 +29,44 @@ const Home: NextPage = () => {
             animate={{ scale: 1 }}
             transition={{ delay: 0.3 }}
           >
-            <button
-              className="rounded bg-black py-2 px-4 font-semibold text-white outline-none transition duration-200 ease-in-out hover:bg-black/60 focus:bg-black/60"
-              onClick={() => {
-                signIn("airtable").catch((e) => {
-                  console.log(e);
-                });
-              }}
-            >
-              Login with Airtable
-            </button>
+            {session.data?.user ? (
+              <span>Logged in as {session.data.user.id}</span>
+            ) : (
+              <button
+                className="rounded bg-black py-2 px-4 font-semibold text-white outline-none transition duration-200 ease-in-out hover:bg-black/60 focus:bg-black/60"
+                onClick={() => {
+                  signIn("airtable").catch((e) => {
+                    console.log(e);
+                  });
+                }}
+              >
+                Login with Airtable
+              </button>
+            )}
           </motion.div>
         </div>
       </main>
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps<{ msg?: string }> = async (
+  context
+) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+  if (session?.user) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {
+      msg: "hello",
+    },
+  };
 };
 
 export default Home;
