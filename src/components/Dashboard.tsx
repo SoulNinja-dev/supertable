977adeae1cd8c431efff061cc7d1fbe161fadd98
pybash1 <67195650+ax-a-dev@users.noreply.github.com>
2023-04-04 18:52:16 +0530

    fix(fine-tuning): fine tune dashboard, fix minor ui bugs, small ui improvements, added loaders, etc

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -31,8 +31,32 @@ const DashboardComponent = () => {
           />
         </div>
       </div>
-      {data &&
-        data.bases.map((base) => <BaseSection key={base.id} {...base} />)}
+      {data ? (
+        data.bases.map((base) => <BaseSection key={base.id} {...base} />)
+      ) : (
+        <div className="flex h-96 w-full items-center justify-center">
+          <svg
+            className="-ml-1 mr-3 h-6 w-6 animate-spin text-[#a09c9b]"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-100"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              className="opacity-50"
+              fill="#000"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </div>
+      )}
     </div>
   );
 };
@@ -42,12 +66,43 @@ const BaseSection: React.FC<BaseObject> = ({ id: baseId, name }) => {
   return (
     <div>
       <div className="mb-4 text-2xl font-semibold">{name}</div>
-      <div className="flex flex-row flex-wrap items-stretch gap-6">
-        {data &&
-          data.tables.map(({ name, id, description }) => (
-            <TableCard baseId={baseId} name={name} desc={description} id={id} key={id} />
-          ))}
-      </div>
+      {data ? (
+        <div className="flex flex-row flex-wrap items-stretch gap-6">
+          {data &&
+            data.tables.map(({ name, id, description }) => (
+              <TableCard
+                baseId={baseId}
+                name={name}
+                desc={description}
+                id={id}
+                key={id}
+              />
+            ))}
+        </div>
+      ) : (
+        <div className="flex h-96 w-full items-center justify-center">
+          <svg
+            className="-ml-1 mr-3 h-6 w-6 animate-spin text-[#a09c9b]"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-100"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              className="opacity-50"
+              fill="#000"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </div>
+      )}
     </div>
   );
 };
