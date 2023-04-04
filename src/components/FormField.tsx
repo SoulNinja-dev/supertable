791e43f594baf755fb4c845b791d0c4c1d636578
milan090 <milanmuhammed1@gmail.com>
2023-04-04 21:21:07 +0530

    feat: animation for formField and helpText api integration

--- src/components/FormField.tsx
+++ src/components/FormField.tsx
@@ -1,6 +1,8 @@
 import classNames from "classnames";
+import { AnimatePresence, motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
 import OutsideClickHandler from "react-outside-click-handler";
+import { useDebounceCallback } from "~/hooks/useDelayedRequest";
 import { FullFormObject } from "~/models/form";
 import { Field } from "~/models/table";
 import { useFormStore } from "~/stores/formStore";
@@ -23,6 +25,7 @@ const FormField = React.forwardRef<HTMLDivElement, FormFieldProps>(
       state.setFormField,
     ]);
     const [isActive, setIsActive] = useState(false);
+    const debouceHelpText = useDebounceCallback(500)
 
     useEffect(() => {
       if (isActive) {
@@ -39,6 +42,20 @@ const FormField = React.forwardRef<HTMLDivElement, FormFieldProps>(
         formId: form.id,
         fieldId: field.id,
         required: value,
+      });
+    };
+
+    const handleHelpTextChange = async (value: string) => {
+      setFormField({
+        ...formField,
+        helpText: value,
+      });
+      debouceHelpText(async () => {
+        await editForm({
+          formId: form.id,
+          fieldId: field.id,
+          helpText: value,
+        });
       });
     };
 
@@ -60,29 +77,64 @@ const FormField = React.forwardRef<HTMLDivElement, FormFieldProps>(
           })}
           onClick={() => setIsActive(true)}
         >
-          {isActive && (
-            <div className="-mt-1 flex w-full justify-end border-b border-b-gray-300 px-2 text-xs font-semibold text-gray-500">
-              <div className="flex items-center">
-                <span>Required</span>
-                <div className="w-8 scale-[0.5]">
-                  <ToggleButton
-                    value={formField.required}
-                    onValueChange={handleToggleRequired}
-                  />
+          <AnimatePresence>
+            {isActive && (
+              <motion.div
+                key="required"
+                initial={{ opacity: 0, height: 0 }}
+                animate={{ opacity: 1, height: "auto" }}
+                exit={{ opacity: 0, height: 0 }}
+                transition={{ duration: 0.2, ease: "easeInOut" }}
+                className="-mt-1 flex w-full justify-end border-b border-b-gray-300 px-2 text-xs font-semibold text-gray-500"
+              >
+                <div className="flex items-center">
+                  <span>Required</span>
+                  <div className="w-8 scale-[0.5]">
+                    <ToggleButton
+                      value={formField.required}
+                      onValueChange={handleToggleRequired}
+                    />
+                  </div>
                 </div>
-              </div>
-            </div>
-          )}
+              </motion.div>
+            )}
+          </AnimatePresence>
+
           <div className="flex w-full flex-col gap-y-5 p-4">
             <div className="flex items-center justify-between">
               <label className="h-5 text-xl">{field.name}</label>
-              {formField.required && (
-                <span className="h-5 text-3xl text-red-500">*</span>
+              {(
+                <span className={classNames({
+                  "h-5 text-3xl text-red-500": true,
+                  "opacity-0": isActive || !formField.required
+                })}>*</span>
               )}
             </div>
+            <AnimatePresence>
+              {isActive && (
+                <motion.div
+                  key="input-help-text"
+                  initial={{ opacity: 0, height: 0 }}
+                  animate={{ opacity: 1, height: "40px" }}
+                  exit={{ opacity: 0, height: 0 }}
+                  transition={{ duration: 0.2, ease: "easeInOut" }}
+                >
+                  <input
+                    type="text"
+                    className="w-full bg-white px-4 py-2"
+                    placeholder="Enter Help Text"
+                    value={formField.helpText || ""}
+                    onChange={(e) => handleHelpTextChange(e.target.value)}
+                  />
+                </motion.div>
+              )}
+            </AnimatePresence>
             <input
               type="text"
               className="w-ful h rounded bg-sidebar px-4 py-3 ring-2 ring-gray-300 transition-all duration-200 ease-in hover:ring-gray-400 focus:outline-none focus:ring-black"
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
             />
           </div>
         </div>
