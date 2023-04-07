3e632a780f2f69141d178cfc7c6539197e613e8d
pybash1 <67195650+ax-a-dev@users.noreply.github.com>
2023-04-07 18:48:33 +0530

    fix(models): extra model for gssp compatibility

--- src/models/form.ts
+++ src/models/form.ts
@@ -22,6 +22,27 @@ export const FormObjectValidator = z.object({
   logo: z.string().nullable(),
 })
 
+export const FormObjectValidatorGSSPCompatible = z.object({
+  id: z.string(),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+
+  tableId: z.string(),
+  title: z.string().nullable(),
+  description: z.string().nullable(),
+  slug: z.string().nullable(),
+  seoDescription: z.string().nullable(),
+  seoImage: z.string().nullable(),
+  headerImage: z.string().nullable(),
+  connectWallet: z.boolean(),
+  theme: z.enum(["classic", "dark", "monochromatic"]),
+  themeColor: z.string().nullable(),
+  submitMsg: z.string().nullable(),
+  contraints: z.string().nullable(),
+  coverImage: z.string().nullable(),
+  logo: z.string().nullable(),
+})
+
 export type FormObject = z.infer<typeof FormObjectValidator>;
 
 export const FullFormObjectValidator = FormObjectValidator.and(z.object({
@@ -46,3 +67,15 @@ export const FormPageObjectValidator = FormObjectValidator.and(z.object({
 }))
 
 export type FormPageObject = z.infer<typeof FormPageObjectValidator>;
+
+export const FormPageObjectValidatorGSSPCompatible = FormObjectValidatorGSSPCompatible.and(z.object({
+  fields: z.array(z.object({
+    fieldId: z.string(),
+    index: z.number(),
+    required: z.boolean(),
+    helpText: z.string().nullable(),
+    field: FieldValidator,
+  }))
+}))
+
+export type FormPageObjectGSSPCompatible = z.infer<typeof FormPageObjectValidatorGSSPCompatible>;
